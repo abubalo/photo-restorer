d4ab912001a622f0ac135976eeba29be7a7e7271
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-15 21:39:32 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    REPLICATE_API_TOKEN: "e82ea9315597f3f1492e866d0ab2f80d1bd5a660"
+  }
 }
 
 module.exports = nextConfig
