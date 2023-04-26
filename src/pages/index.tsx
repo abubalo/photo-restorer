56ff461bab652d261bceee1f620d532b3030368d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-26 05:16:35 +0200

    imported modal and remove input file element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,82 @@
-import {useState} from "react"
+import { useEffect, useState } from "react";
 import Link from "next/link";
-import Modal from "./Modal";
-import DropZone from "./DropZone";
-import axios from "axios"
+import Modal from "../components/Modal";
+import axios from "axios";
+import Loader from "../components/Loader";
+import Head from "next/head";
+import UploadWidget from "../components/UploadWidget";
+import ErrorMessage from "../components/ErrorMessage";
 export default function Home() {
+  const [originalImg, setOriginalImg] = useState<File | null>();
+  const [restoredImg, setRestoredImg] = useState<File | null>();
 
-  const [preview, setPreview] = useState("");
-  const [file, setFile] = useState<File | null>(null);
-  const [onClose, setOnclose] = useState(false)
+  const [onClose, setOnClose] = useState(false);
+  const [onError, setOnError] = useState(true);
 
-  async function handleFileUpload(){
-    // const file = e.target.files[0];
-    const data = new FormData();
-    try{
-      await axios.post("/api/server", data)
-    }catch(error: any){
+  async function handleFileUpload() {
+    try {
+      await axios.post("/api/server");
+    } catch (error: any) {
       console.log(error.message);
     }
-    
   }
 
   return (
     <main className="">
-      <header className="w-[90%] fixed container left-0 right-0 top-0 mx-auto flex justify-between items-center p-4">
-        <div className="text-2xl font-bold">PixelRevive.io</div>
+      <Head>
+        <title>PixelRevivr</title>
+        <meta name="description" content="Effortlessly restore images" />
+        <meta name="author" content="Abubakar Balogun" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      </Head>
+      <header className="w-[90%] fixed container left-0 right-0 top-0 mx-auto flex justify-between items-center p-4 z-50">
+        <div className="text-2xl font-bold">PixelRevivr.io</div>
         <nav className="flex space-x-6">
-          <Link href="https://github.com/abubalo/photo-restorer.git">Github</Link>
+          <Link
+            href="https://github.com/abubalo/photo-restorer.git"
+            target={"_blank"}
+          >
+            Github
+          </Link>
+          <Link
+            href=""
+            onClick={() => setOnClose(true)}
+            className="border hover:border-gray-500"
+          >
+            {onClose ? "Button" : "Modal"}
+          </Link>
+          <Link
+            href=""
+            onClick={() => setOnError(true)}
+            className="border hover:border-gray-500"
+          >
+            Error
+          </Link>
         </nav>
       </header>
-      <div className="container mx-auto h-screen flex flex-col justify-center items-center mt-10 space-y-12">
-       
+      <div className="relative container mx-auto h-screen flex flex-col justify-center items-center  space-y-12 overflow-hidden">
         <div className="text-center space-y-4">
           <h1 className="text-7xl font-bold">Effortlessly Restore Images</h1>
           <p className=" md:w-3/4 mx-auto md:text-lg">
-          Say goodbye to blurry, faded, and damaged images. Our app restores your photos to their original quality and beyond.
+            Say goodbye to blurry, faded, and damaged images. <strong>PixelRevivr.io</strong> restores
+            your photos to their original quality and beyond.
           </p>
         </div>
-        <label
-          htmlFor="file"
-          className="w-1/2 h-52 p-4 flex flex-col justify-center items-center border-dashed border-2 border-gray-800 rounded-md cursor-pointer hover:bg-slate-900 hover:bg-opacity-50 transition-all ease-linear dark:border-neutral-800/50"
-        >
-          <p className="px-5 py-3 rounded-md bg-neutral-800/50">Upload a photo to restore here</p>
-          <p className="p-1 text-sm">Drag the photo here</p>
-          <input type="file" name="file" id="file" hidden />
-          <DropZone />
-        </label>
-  
+
+        <UploadWidget setOriginalImg={setOriginalImg} setRestoredImg={setRestoredImg}/>
       </div>
-      <div className="grid grid-cols-2"></div>
-      <Modal originalImg={""} restoredImg={""}/>
+
+      <Modal
+        originalImg={"/img2.jpg"}
+        restoredImg={"/img2.jpg"}
+        setOnClose={setOnClose}
+        onClose={onClose}
+      />
+      <ErrorMessage
+        message={"Something went wrong"}
+        onError={onError}
+        setOnError={setOnError}
+      />
     </main>
   );
 }
