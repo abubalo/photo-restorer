fb672c8d2329f7c7bb50b152b902bc58124d14a6
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-26 04:53:15 +0200

    Modified next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    REPLICATE_API_TOKEN: "e82ea9315597f3f1492e866d0ab2f80d1bd5a660"
+    REPLICATE_API_TOKEN: "e82ea9315597f3f1492e866d0ab2f80d1bd5a660",
   }
 }
 
