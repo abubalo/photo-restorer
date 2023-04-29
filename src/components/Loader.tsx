2b598193c5651b465f2ba8f637a34c959e9c83ff
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-29 22:26:37 +0200

    Minor changes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ const Loader = () => {
   return (
     <div className="fixed  top-0 left-0 right-0 bottom-0 flex flex-col gap-2 items-center justify-center select-none">
       <div className="w-12 h-12 border-t-4 border-b-4 dark:border-white rounded-full animate-spin"></div>
-      <div className="text-lg">Please wait...</div>
+      <div className="text-lg">Please wait while you request is processing...</div>
     </div>
   );
 };
