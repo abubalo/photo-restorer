f8f8a642613bcdc81eaecaff5d866e0166878359
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-29 22:31:25 +0200

    refacer server.ts and fixed request error

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -1,50 +1,50 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 import Replicate from "replicate";
+ 
 
-interface PromptUrl {
-  version: string;
-  input:{
-    imageUrl: string;
 
-  }
-}
 
-interface ExtendedNextApiRequest extends NextApiRequest {
-  body: {
-    imageUrl: string;
-  };
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<string>
-) {
-  const imageUrl = req.body.formData;
-
-  if (!imageUrl) {
-    res.status(401).json("Invlid input");
-  }
-
-  const ApiToken = process.env.REPLICATE_API_TOKEN;
-
-  if (!ApiToken) {
-    throw new Error("API Token is undifined");
-  }
-
-  const replicate = new Replicate({ auth: ApiToken });
-
-  try {
-    const output: any = await replicate.run(
-      "cjwbw/vqfr:ccd53a9a38ebbaa783a1e6318d22fa68c14c3aed66cc3589e53ef07d07f5be1d",
-      {
-        input: {
-          image: imageUrl,
-        },
-      }
-    );
-
-    res.status(201).json(output);
-  } catch (error: any) {
-    res.status(500).json(error);
-  }
-}
+ export default async function handler(
+   req: NextApiRequest,
+   res: NextApiResponse
+ ) {
+   
+ 
+   const apiToken = process.env.REPLICATE_API_TOKEN;
+ 
+   if (!apiToken) {
+     throw new Error("API Token is missing");
+   }
+   
+   const {imageUrl} = req.body
+   res.json(imageUrl)
+    // configure api key
+   const replicate = new Replicate({ auth: apiToken });
+   const model =
+     "cjwbw/vqfr:ccd53a9a38ebbaa783a1e6318d22fa68c14c3aed66cc3589e53ef07d07f5be1d";
+ 
+     try {
+       const output: any = await replicate.run(model, {
+         input: {
+           image: imageUrl,
+         },
+       });
+       // Check if the response has already been sent
+       if (!res.headersSent) {
+         res.json(output);
+       }
+     } catch (error: any) {
+       if (error.response) {
+         // Check if the response has already been sent
+         if (!res.headersSent) {
+           res.status(error.response.status).json(error.response.statusText);
+         }
+       } else {
+         // Check if the response has already been sent
+         if (!res.headersSent) {
+           res.json({ message: error.message });
+         }
+       }
+     }
+     
+ }
