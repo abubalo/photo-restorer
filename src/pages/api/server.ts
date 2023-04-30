357c2f23ee67e665eba1a9fb7ea1e491d604c509
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-30 21:12:02 +0200

    refactor server.ts be removing unnecessary if statements

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -1,50 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 import Replicate from "replicate";
- 
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const apiToken = process.env.REPLICATE_API_TOKEN;
 
+  if (!apiToken) {
+    throw new Error("API Token is missing");
+  }
 
- export default async function handler(
-   req: NextApiRequest,
-   res: NextApiResponse
- ) {
-   
- 
-   const apiToken = process.env.REPLICATE_API_TOKEN;
- 
-   if (!apiToken) {
-     throw new Error("API Token is missing");
-   }
-   
-   const {imageUrl} = req.body
-   res.json(imageUrl)
-    // configure api key
-   const replicate = new Replicate({ auth: apiToken });
-   const model =
-     "cjwbw/vqfr:ccd53a9a38ebbaa783a1e6318d22fa68c14c3aed66cc3589e53ef07d07f5be1d";
- 
-     try {
-       const output: any = await replicate.run(model, {
-         input: {
-           image: imageUrl,
-         },
-       });
-       // Check if the response has already been sent
-       if (!res.headersSent) {
-         res.json(output);
-       }
-     } catch (error: any) {
-       if (error.response) {
-         // Check if the response has already been sent
-         if (!res.headersSent) {
-           res.status(error.response.status).json(error.response.statusText);
-         }
-       } else {
-         // Check if the response has already been sent
-         if (!res.headersSent) {
-           res.json({ message: error.message });
-         }
-       }
-     }
-     
- }
+  const { imageUrl } = req.body;
+  // configure api key
+  const replicate = new Replicate({ auth: apiToken });
+  const model =
+    "cjwbw/vqfr:ccd53a9a38ebbaa783a1e6318d22fa68c14c3aed66cc3589e53ef07d07f5be1d";
+
+  try {
+    const output: any = await replicate.run(model, {
+      input: {
+        image: imageUrl,
+      },
+    });
+    res.json(output);
+  } catch (error: any) {
+    res.json(error);
+  }
+}
