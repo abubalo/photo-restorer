3063f38f50656e21a62676b830ec73e59374f2e1
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-01 08:22:36 +0200

    Removed api keys from next.config for robust security

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    REPLICATE_API_TOKEN: "e82ea9315597f3f1492e866d0ab2f80d1bd5a660",
-    S3_ACCESS_KEY: "AKIA2A6WW5RRT2ROQ3KD",
-    S3_SECRET_ACCESS_KEY: "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"
-  }
 }
 
 module.exports = nextConfig
