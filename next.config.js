5a9bd2f690cb5dc41dfab6f74755f3e84423942c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-07-25 21:48:40 +0200

    Add aws domain to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['photo-restorer.s3.eu-north-1.amazonaws.com'],
+  }
 }
 
 module.exports = nextConfig
