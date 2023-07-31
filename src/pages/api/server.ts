13a70c2f9b7f3b71dde953d697a63c1676a3139f
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-07-31 23:34:17 +0200

    refactor: add response status

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -1,18 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import Replicate from "replicate";
+import Replicate from "replicate"; 
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const imageUrl = req.body.imageUrl;
+
   const apiToken = process.env.REPLICATE_API_TOKEN;
 
   if (!apiToken) {
     throw new Error("API Token is missing");
   }
 
-  const { imageUrl } = req.body;
-  // configure api key
   const replicate = new Replicate({ auth: apiToken });
   const model =
     "cjwbw/vqfr:ccd53a9a38ebbaa783a1e6318d22fa68c14c3aed66cc3589e53ef07d07f5be1d";
@@ -23,8 +29,8 @@ export default async function handler(
         image: imageUrl,
       },
     });
-    res.json(output);
+    res.status(200).json(output);
   } catch (error: any) {
-    res.json(error);
+    res.status(500).json(error);
   }
 }
